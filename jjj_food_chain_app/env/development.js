f9000c3fe6960dcfac0c26fdfccacf07e0aec158
0218Ling <3234131843@qq.com>
2024-10-26 18:03:08 +0800

    111

--- jjj_food_chain_app/env/development.js
+++
@@ -1,7 +0,0 @@
-let url = 'http://127.0.0.1:8891';
-if(process.env.NODE_ENV != 'development'){
-	url = '/api';
-}
-export default {
-	url
-}
--- jjj_food_chain_shop/src/api/link.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-let LinkApi = {
-    /*获取数据列表*/
-    getPageList(data, errorback) {
-        return request._post('/shop/data/link/getPageList', data, errorback);
-    },
-}
-export default LinkApi;
